ab58b709c4511016437bea38c76123fe88cb511d
bugigen <109750195+bugigen@users.noreply.github.com>
2025-05-31 18:40:52 +0400

    Add files via upload

---
+++ Practice5-2/Practice5.Task1/Contractor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Practice5.Task1
+{
+    public class Contractor : Employee
+    {
+        public Contractor(string name, int salary, float hourlyRate) : base(name, salary)
+        {
+            HourlyRate = hourlyRate;
+        }
+
+        public float HourlyRate { get; private set; }
+
+        public new float CalculateBonus(int hoursWorked)
+        {
+            float cashback = Salary * HourlyRate * hoursWorked;
+
+            return cashback;
+        }
+    }
+}
---
+++ Practice5-2/Practice5.Task1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Practice5.Task1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new("Bob", 1000);
+            Manager manager = new("John", 2000, 7);
+            Contractor contractor = new("Kenny", 3000, 0.01f);
+
+            Console.WriteLine($"Зарплата рабочего: {employee.Salary}, кэшбэк рабочего: {employee.CalculateBonus()}");
+            Console.WriteLine($"Зарплата менеджера: {manager.Salary}, кэшбэк менеджера: {manager.CalculateBonus()}");
+            Console.WriteLine($"Зарплата контрактника: {contractor.Salary}, кэшбэк контрактника {contractor.CalculateBonus(8)}");
+        }
+    }
+}
